66dbc76653ec398a467fb8ed81f9c21d7041c4ea
Natttalla <145061080+Natttalla@users.noreply.github.com>
2024-10-06 10:11:43 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function klepsydra() {
+    document.body.style.cursor = 'wait';
+  }
+  
+  function znak() {
+    document.body.style.cursor = 'help';
+  }
+  
+  function wskaznik() {
+    document.body.style.cursor = 'pointer';
+  }
+  
+  function przesuwanie() {
+    document.body.style.cursor = 'move';
+  }
+  
+  function piwo() {
+    document.body.style.cursor = 'url("piwo.png"), pointer';
+  }
+  
+  function ksiazka() {
+    document.body.style.cursor = 'url("ksiazka.png"), pointer';
+  }
